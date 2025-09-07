64aaa4c00dc6e3c236d67acbdd3c59004bcba27a
ahere3a <126818981+Ahere3a@users.noreply.github.com>
2025-09-07 10:36:41 +0300

    Update 06_dear_diary.py

--- 2-file-and-error-handling/06_dear_diary.py
+++ 2-file-and-error-handling/06_dear_diary.py
@@ -4,3 +4,12 @@
 with open('diaries.txt', 'w') as file:
   file.write('Date: March 19, 2024 \n')
   file.write('Dear Diary...')
+  #file = open('diary.txt', 'w')
+#file.write('Dear Diary,\n')
+file.write('im having a lot in mind mahn.\n')
+file.write('I hope I can sort it out.\n')
+file.write('im feeling sad like really in my head about this but i hope it gets better.\n ')
+file.write('I just want to be happy and free.\n')
+file.write('i always feel better when i write codes that work but then if the code does not run it adds to the frustration yk?.\n')
+file.close()
+file = open('diary.txt', 'r')
